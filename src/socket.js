3345c5341fc1f3b832062b98c1be4edd5f299b25
retropiaf <sawadogochristiane@gmail.com>
2018-04-18 10:19:40 -0700

    listen for event

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,16 @@
 const db = require('./db');
 
+
 const onSocketConnect = io => socket => {
 
+
+
   // TODO 2.1 Listen for login events (eg "LOGIN") from client and save the user using db.create(username, socket.id)
+
+
   // TODO 2.2 Prevent users from using an existing username using the "acknowledgement" from the client
+
+
   // TODO 2.3 Emit an update user list event (eg "UPDATE_USER_LIST") to all clients when there is a login event
   // TODO 2.4 Listen for "disconnect" events and remove the socket user from the users object (*hint: db.create(username, socket.id) returns the logout fn)
   // TODO 2.5 emit "UPDATE_USER_LIST" after user has been "logged out" and is removed from "users" object
@@ -12,14 +19,24 @@ const onSocketConnect = io => socket => {
   // TODO 3.2 Include information about the "fromUser" so the client can filter draw events from other users and only display events from the selected user
 
   // TODO 1.4 listen for draw action-type events (eg "DRAW_POINTS") from the socket and broadcast them to others sockets.
+  socket.on('DRAW_POINTS', data => {
+    socket.broadcast.emit('DRAW_POINTS', data);
+  })
 
 };
 
 const connect = server => {
+
   // TODO 1.1 import socket.io
+
   // TODO 1.2 attach a socket to the express server by passing the express server instance as an argument when socket.io is invoked
+  const io = require('socket.io')(server).listen(3000);
+
 
   // TODO 1.3 listen for new connections and use the provided "onSocketConnect" function
+  io.on('connection', socket => {
+    onSocketConnect(socket);
+  })
 }
 
 module.exports = connect;
